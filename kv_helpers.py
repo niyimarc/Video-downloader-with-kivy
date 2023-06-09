9c481aef204f9afcc995e7e0ffdd7043175f8ec7
Drey <oluwadamilareadeniye@yahoo.com>
2023-06-09 14:05:34 -0700

    Created a youtube downloader for mobile screen

---
+++ kv_helpers.py
@@ -0,0 +1,132 @@
+screen_helper = """
+MDScreen:
+    MDNavigationLayout:
+        ScreenManager:
+            Screen:
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDTopAppBar:
+                        title: "Demo App"
+                        elevation: 4
+                        pos_hint: {"top": 1}
+                        specific_text_color: "#4a4939"
+                        left_action_items:
+                            [['menu', lambda x: nav_drawer.set_state("open")]]
+
+                    MDBottomNavigation:
+                        #panel_color: "#eeeaea"
+                        selected_color_background: "orange"
+                        text_color_active: "lightgrey"
+                        
+                        
+                        MDBottomNavigationItem:
+                            name: 'screen 3'
+                            text: 'Home'
+                            icon: 'home'
+                
+                            MDLabel:
+                                text: 'Home'
+                                halign: 'center'
+                                
+                        MDBottomNavigationItem:
+                            name: 'screen 1'
+                            text: 'Youtube'
+                            icon: 'youtube'
+                
+                            Image:
+                                source: 'img/youtube.png'
+                                size_hint: (.5,None)
+                                pos_hint: {'center_x': 0.5, 'center_y': 0.9}
+                                
+                                
+                            MDTextField:
+                                id: link_input
+                                hint_text: "Paste Youtube Video Link"
+                                mode: "round"
+                                helper_text: "Paste Youtube Video Link"
+                                pos_hint: {'center_x': 0.4, 'center_y': 0.75}
+                                size_hint: (.7,None)
+                                
+                            MDRoundFlatButton:
+                                text: "Get Link"
+                                pos_hint: {'center_x': 0.86, 'center_y': 0.75}
+                                size_hint: (None,None)
+                                on_release: app.getLinkInfo(self)  # Call the function when the button is released
+                                
+                            
+                            
+                            BoxLayout:
+                                id: video_details
+                                orientation: "horizontal"
+                                pos_hint: {'center_x': 0.5, 'center_y': 20}
+                                
+                                AsyncImage:
+                                    id: async_image
+                                    size_hint: (.5,None)
+                                    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+                                    source:''
+                                    
+                                BoxLayout:
+                                    orientation: "vertical"
+                                    size_hint: (.5, None)
+                                    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+
+                                    MDLabel:
+                                        id: video_title
+                                        text: ''
+                                        size: self.texture_size
+                                        
+                                        
+                                    
+                                    BoxLayout:        
+                                        orientation: "horizontal"   
+                                        spacing: "10dp"  # Set the desired spacing              
+                                        MDRoundFlatButton:
+                                            text: "Download"
+                                            size_hint: (None,None)    
+                                            on_release: app.downloadVideo(self)  # Call the function when the button is released    
+                                        
+                                        MDTextFieldRect:
+                                            id: drop_down_btn
+                                            text: "Resolution"
+                                            size_hint: (None,0.7)  
+                                            on_focus: if self.focus: app.show_resolution_menu(self)  # Show resolution menu on focus      
+                                        
+                                        DropDown:
+                                            id: drop_down              
+                                            
+        
+                        MDBottomNavigationItem:
+                            name: 'screen 2'
+                            text: 'Facebok'
+                            icon: 'facebook'
+                
+                            MDLabel:
+                                text: 'Facebook'
+                                halign: 'center'
+                
+                                    
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+            MDNavigationDrawerMenu:
+    
+                MDNavigationDrawerHeader:
+                    title: "Header title"
+                    title_color: "#4a4939"
+                    text: "Header text"
+                    spacing: "4dp"
+                    padding: "12dp", 0, 0, "56dp"
+    
+                MDNavigationDrawerLabel:
+                    text: "Video Downloader"
+                    
+                MDNavigationDrawerItem
+                    icon: "youtube"
+                    text: "Youtube Downloader"
+                
+                MDNavigationDrawerItem
+                    icon: "facebook"
+                    text: "Facebok Downloader"                
+ 
+"""
---
+++ main.py
@@ -0,0 +1,67 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivymd.uix.button import MDRectangleFlatButton
+from kv_helpers import screen_helper
+from pytube import YouTube
+
+
+
+Window.size = (450,700)
+class DemoApp(MDApp):
+
+    def build(self):
+        self.theme_cls.primary_palette = "Orange"
+        screen = Builder.load_string(screen_helper)
+        return screen
+
+    def getLinkInfo(self, event):
+        self.link = self.root.ids.link_input.text  # Access MDTextField using ids
+        self.yt = YouTube(self.link)
+        self.source = self.yt.thumbnail_url
+        self.vtitle = self.yt.title
+
+        video_details = self.root.ids.video_details # Access the BoxLayout of video details
+        video_details.pos_hint = {'center_x': 0.5, 'center_y': 0.6}
+
+        async_image = self.root.ids.async_image  # Access AsyncImage using ids
+        async_image.source = self.source
+
+        video_title = self.root.ids.video_title
+        video_title.text = self.vtitle
+
+        self.video = self.yt.streams.filter(file_extension='mp4').order_by('resolution').desc()
+        drop_down = self.root.ids.drop_down
+        drop_down.clear_widgets()  # Clear existing resolution buttons
+
+    def show_resolution_menu(self, instance):
+        drop_down = self.root.ids.drop_down
+        drop_down.clear_widgets()  # Clear existing resolution buttons
+
+        for video in self.video:
+            btn = MDRectangleFlatButton(
+                text=video.resolution,
+                size_hint= (None,None),
+                on_release=lambda btn: self.select_resolution(btn.text),
+            )
+            drop_down.add_widget(btn)
+
+        drop_down.open(instance)
+
+    def select_resolution(self, resolution):
+        self.root.ids.drop_down_btn.text = resolution
+
+    def downloadVideo(self, event):
+        resolution = self.root.ids.drop_down_btn.text  # Get the selected resolution
+        if resolution != 'Resolution':
+            selected_video = self.video.filter(res=resolution).first()
+            if selected_video:
+                selected_video.download("YoutubeDownloader")
+                print('Download Completed')
+            else:
+                print('Selected resolution is not available for download')
+        else:
+            print('Please select a resolution')
+
+if __name__ == "__main__":
+    DemoApp().run()
