c261a253bd7a23b478f1bee6ffab4cb7a994dbc5
Drey <oluwadamilareadeniye@yahoo.com>
2023-06-10 06:52:41 -0700

    Modified the internet connection checked

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.snackbar import Snackbar
+
 from kv_helpers import screen_helper
 from pytube import YouTube
 import socket
@@ -24,14 +26,7 @@ class DreyApp(MDApp):
         # Check for internet connection
         if not self.check_internet_connection():
             # Show dialog if there is no internet connection
-            if not self.dialog:
-                self.dialog = MDDialog(
-                    title="No internet connection",
-                    type="simple",
-                    text="Oops! Check your internet connection and try again!",
-                    radius=[20, 7, 20, 7],
-                )
-            self.dialog.open()
+            self.show_no_internet_dialog()
             return
 
         self.link = self.root.ids.link_input.text  # Access MDTextField using ids
@@ -55,17 +50,6 @@ class DreyApp(MDApp):
         drop_down = self.root.ids.drop_down
         drop_down.clear_widgets()  # Clear existing resolution buttons
 
-    def check_internet_connection(self):
-        try:
-            # Create a socket connection to a well-known host
-            # Create a socket connection to Google's DNS server (8.8.8.8 on port 53).
-            # If the connection is successful, it means there is an internet connection.
-            # If an error occurs, it means there is no internet connection.
-            socket.create_connection(("8.8.8.8", 53))
-            return True
-        except socket.error:
-            return False
-
     def show_resolution_menu(self, instance):
         drop_down = self.root.ids.drop_down
         drop_down.clear_widgets()  # Clear existing resolution buttons
@@ -85,15 +69,72 @@ class DreyApp(MDApp):
 
     def downloadVideo(self, event):
         resolution = self.root.ids.drop_down_btn.text  # Get the selected resolution
+
+        if not self.check_internet_connection():
+            # Show dialog if there is no internet connection
+            self.show_no_internet_dialog()
+            return
+
         if resolution != 'Resolution':
             selected_video = self.video.filter(res=resolution).first()
             if selected_video:
                 selected_video.download("YoutubeDownloader")
-                print('Download Completed')
+                print('Downloading')
+                Snackbar(
+                    text="[color=#ddbb34]Downloading video![/color]",
+                    snackbar_x="10dp",
+                    snackbar_y="10dp",
+                    size_hint_x=.7,
+                ).open()
+
             else:
-                print('Selected resolution is not available for download')
+                Snackbar(
+                    text="[color=#ddbb34]Selected resolution is not available for download![/color]",
+                    snackbar_x="10dp",
+                    snackbar_y="10dp",
+                    size_hint_x=.7,
+                ).open()
         else:
-            print('Please select a resolution')
+            Snackbar(
+                text="[color=#ddbb34]please select a resolution before download![/color]",
+                snackbar_x="10dp",
+                snackbar_y="10dp",
+                size_hint_x=.7,
+            ).open()
+
+    def video_download_complete(self, stream, file_path):
+        Snackbar(
+            text="[color=#ddbb34]Download completed![/color]",
+            snackbar_x="10dp",
+            snackbar_y="10dp",
+            size_hint_x=.7,
+        ).open()
+
+    def check_internet_connection(self):
+        try:
+            # Create a socket connection to a well-known host
+            # Create a socket connection to Google's DNS server (8.8.8.8 on port 53).
+            # If the connection is successful, it means there is an internet connection.
+            # If an error occurs, it means there is no internet connection.
+            socket.create_connection(("8.8.8.8", 53))
+            return True
+        except socket.error:
+            return False
+
+    def show_no_internet_dialog(self):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                title="No internet connection",
+                type="simple",
+                text="Oops! Check your internet connection and try again!",
+                radius=[20, 7, 20, 7],
+            )
+        if not self.dialog._is_open:
+            self.dialog.open()
+
+    def on_stop(self):
+        if self.dialog:
+            self.dialog.dismiss()
 
 if __name__ == "__main__":
     DreyApp().run()
